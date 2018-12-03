1704103cf0d4567720f9d37b2ade18bca37447f0
EC2 Default User <ec2-user@ip-172-31-38-58.ec2.internal>
2018-12-03 00:57:54 +0000

    Added AWS Kinesis consumer that saves to DynamoDB
    There is some space for improvement in processing the tweets such
    that tweets get saved after being cleaned further

--- data/createKinesis.py
+++
@@ -1,8 +0,0 @@
-# Author: Hyeong Joon Suh
-# Filename: createKinesis.py
-# Description: use boto3 library to create AWS Kinesis Stream
-
-import boto3
-
-client = boto3.client("kinesis")
-client.create_stream(StreamName="twitter", ShardCount=1)
