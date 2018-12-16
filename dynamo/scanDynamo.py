877ebbd01b833db6cabd1800e7a4fa021ec13376
Hyeong Joon Suh <hjsuh18@gmail.com>
2018-12-17 07:20:18 +0900

    Changed scanDynamo to scan whole table

--- dynamo/scanDynamo.py
+++ dynamo/scanDynamo.py
@@ -3,20 +3,32 @@
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
 
-dynamodb = boto3.client('dynamodb')
-
-print("List of tables:")
-print(dynamodb.list_tables())
-
+dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('tweets')
-print("tweets table:")
-print(table)
-
-response = table.scan(
-        FilterExpression=Attr('sarcastic`').eq(true)
-        )
-items = response['Items']
+print("tweets table item count:")
+print(table.item_count)
 
 print("Number of sarcastic tweets:")
-print(len(items))
+count = 0
 
+response = table.scan(
+        FilterExpression=Attr('sarcastic').eq('true')
+        )
+data = response['Items']
+for d in data:
+	count = count + 1
+	print(count)
+
+while response.get('LastEvaluatedKey'):
+	print("New Batch")
+	response = table.scan(
+		FilterExpression=Attr('sarcastic').eq(True),
+		ExclusiveStartKey=response['LastEvaluatedKey']
+		)
+	data = response['Items']
+	for d in data:
+		count = count + 1
+		print(count)
+
+print("Final Count:")
+print(count)
