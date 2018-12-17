44e3d348a7b322165d7eaba2343da09b228a0c1b
Hyeong Joon Suh <hjsuh18@gmail.com>
2018-12-17 09:39:35 +0900

    Print sarcastic tweets

--- dynamo/scanDynamo.py
+++ dynamo/scanDynamo.py
@@ -4,7 +4,7 @@ import boto3
 from boto3.dynamodb.conditions import Key, Attr
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('tweets_hashtag')
+table = dynamodb.Table('tweets')
 print("tweets table item count:")
 print(table.item_count)
 
@@ -12,23 +12,29 @@ print("Number of sarcastic tweets:")
 count = 0
 
 response = table.scan(
-        FilterExpression=Attr('sarcastic').eq('true')
+        #FilterExpression=Attr('sarcastic').eq('true')
         )
 data = response['Items']
 for d in data:
-	count = count + 1
-	print(count)
+	text = d['text']
+	if '#sarcasm' in text or '#sarcastic' in text:
+		print text
+	#count = count + 1
+	# print(count)
 
 while response.get('LastEvaluatedKey'):
 	print("New Batch")
 	response = table.scan(
-		FilterExpression=Attr('sarcastic').eq(True),
+		#FilterExpression=Attr('sarcastic').eq(True),
 		ExclusiveStartKey=response['LastEvaluatedKey']
 		)
 	data = response['Items']
 	for d in data:
-		count = count + 1
-		print(count)
+		text = d['text']
+		if '#sarcasm' in text or '#sarcastic' in text:
+			print text
+		#count = count + 1
+		#print(count)
 
-print("Final Count:")
-print(count)
+#print("Final Count:")
+#print(count)
