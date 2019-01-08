bea2459bd836e3d2f5e6bb86db49df1f6de6e01b
Hyeong Joon Suh <hjsuh18@gmail.com>
2019-01-08 01:28:06 -0500

    python script for finding best models

---
+++ reddit/reddit_modeling_subset.py
@@ -0,0 +1,187 @@
+# import trainX, trainY, testX and testY from pickle files
+with open('trainXsubset.pkl', 'rb') as f:
+    trainX = pickle.load(f)
+with open('trainYsubset.pkl', 'rb') as f:
+    trainY = pickle.load(f)
+with open('testXsubset.pkl', 'rb') as f:
+    testX = pickle.load(f)
+with open('testYsubset.pkl', 'rb') as f:
+    testY = pickle.load(f)
+
+from sklearn import metrics
+def evaluation(true, prediction):
+    print("Accuracy: ", metrics.accuracy_score(true, prediction))
+    print("Precision: ", metrics.precision_score(true, prediction))
+    print("Recall: ", metrics.recall_score(true, prediction))
+    print("Confusion Matrix\n", metrics.confusion_matrix(true, prediction))
+    print("F1 score: ", metrics.f1_score(true, prediction))
+
+trainXNoParent = [a[0:5] for a in trainX]
+testXNoParent = [a[0:5] for a in testX]
+
+# SVM
+# print("SVM no parent")
+# from sklearn import svm
+# from sklearn.model_selection import GridSearchCV
+# parameters = {'kernel':('linear', 'rbf'), 'C':[1, 10, 50, 100, 120, 135, 150, 160, 170, 180]}
+# svc = svm.SVC(gamma='scale')
+# clf = GridSearchCV(svc, parameters, cv= 5)
+# clf.fit(trainXNoParent, trainY)
+# prediction = clf.predict(testXNoParent)
+# evaluation(testY, prediction)
+# print(clf.best_estimator_)
+# print()
+
+
+# # In[34]:
+
+
+# # Gaussian Process Classification
+# print("Gaussian Process Classification no parent")
+# from sklearn.gaussian_process import GaussianProcessClassifier
+# from sklearn.gaussian_process.kernels import RBF
+# kernel = 1.0 * RBF(1.0)
+# clf = GaussianProcessClassifier(kernel=kernel,random_state=0)
+# clf.fit(trainXNoParent, trainY)
+# prediction = clf.predict(testXNoParent)
+# evaluation(testY, prediction)
+# print()
+
+
+# In[ ]:
+
+
+# Random Forest Classification
+print("Random Forest Classification no parent")
+from sklearn.model_selection import RandomizedSearchCV
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
+max_features = ['auto', 'sqrt']
+max_depth = [int(x) for x in np.linspace(100, 500, num = 11)]
+max_depth.append(None)
+random_grid = {
+ 'n_estimators': n_estimators,
+ 'max_features': max_features,
+ 'max_depth': max_depth
+ }
+rfc = RandomForestClassifier()
+clf = RandomizedSearchCV(estimator = rfc, param_distributions = random_grid, n_iter = 100, cv = 5, verbose=2, random_state=42, n_jobs = -1)
+# Fit the model
+clf.fit(trainXNoParent, trainY)
+prediction = clf.predict(testXNoParent)
+evaluation(testY, prediction)
+print(clf.best_params_)
+print()
+
+
+# In[ ]:
+
+
+# Neural Network Classification
+print("Neural Network Classification no parent")
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import GridSearchCV
+mlp = MLPClassifier()
+parameter_space = {
+    'hidden_layer_sizes': [(5), (4), (3), (5, 2), (4, 2), (3, 2)],
+    'activation': ['tanh', 'relu'],
+    'solver': ['sgd', 'adam', 'lbfgs'],
+    'alpha': [0.0001, 0.01, 0.001],
+    'learning_rate': ['constant', 'adaptive'],
+}
+clf = GridSearchCV(mlp, parameter_space, n_jobs=-1, cv=5, verbose=True)
+clf.fit(trainXNoParent, trainY)
+prediction = clf.predict(testXNoParent)
+evaluation(testY, prediction)
+print(clf.best_params_)
+print()
+
+
+# ## With Parent Contrast
+
+# In[ ]:
+
+
+# SVM
+print("SVM parent")
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV
+parameters = {'kernel':('linear', 'rbf'), 'C':[1, 10, 50, 100, 120, 135, 150, 160, 170, 180]}
+clf = GridSearchCV(svc, parameters, cv= 5)
+clf.fit(trainX, trainY)
+prediction = clf.predict(testX)
+evaluation(testY, prediction)
+print(clf.best_estimator_)
+print()
+
+
+# In[ ]:
+
+
+# Gaussian Process Classification
+print("Gaussian Process Classification parent")
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+kernel = 1.0 * RBF(1.0)
+clf = GaussianProcessClassifier(kernel=kernel,random_state=0)
+clf.fit(trainX, trainY)
+prediction = clf.predict(testX)
+evaluation(testY, prediction)
+print()
+
+
+# In[ ]:
+
+
+# Random Forest Classification
+print("Random Forest Classification parent")
+import numpy as np
+from sklearn.model_selection import RandomizedSearchCV
+n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
+max_features = ['auto', 'sqrt']
+max_depth = [int(x) for x in np.linspace(100, 500, num = 11)]
+max_depth.append(None)
+random_grid = {
+ 'n_estimators': n_estimators,
+ 'max_features': max_features,
+ 'max_depth': max_depth
+ }
+rfc = RandomForestClassifier()
+clf = RandomizedSearchCV(estimator = rfc, param_distributions = random_grid, n_iter = 100, cv = 5, verbose=2, random_state=42, n_jobs = -1)
+# Fit the model
+clf.fit(trainX, trainY)
+prediction = clf.predict(testX)
+evaluation(testY, prediction)
+print(clf.best_params_)
+print()
+
+
+# In[ ]:
+
+
+# Neural Network Classification
+print("Neural Network Classification parent")
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import GridSearchCV
+parameter_space = {
+    'hidden_layer_sizes': [(7), (6), (5), (4), (3), (7, 3), (7, 4), (7, 2), (6, 4), (6, 3), (6, 2), (5, 3), (5, 2), (4, 2), (3, 2)],
+    'activation': ['tanh', 'relu'],
+    'solver': ['sgd', 'adam', 'lbfgs'],
+    'alpha': [0.0001, 0.01, 0.001],
+    'learning_rate': ['constant', 'adaptive'],
+}
+mlp = MLPClassifier()
+clf = GridSearchCV(mlp, parameter_space, n_jobs=-1, cv=5, verbose=True)
+clf.fit(trainX, trainY)
+prediction = clf.predict(testX)
+evaluation(testY, prediction)
+print(clf.best_params_)
+print()
+
+
+# In[ ]:
+
+
+
+
